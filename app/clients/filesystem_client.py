9d2f5830fb1db43e2c916e531579541878d1554e
Sebastian Winkler <sebastian.winkler@yandex.com>
2020-11-05 18:02:21 +0100

    feat(filesystem): added logging to filesystem_client.py

--- app/clients/filesystem_client.py
+++ app/clients/filesystem_client.py
@@ -1,6 +1,9 @@
 import json
+import logging
 
 from config import OHLCV_CONFIG_FILE
+
+log = logging.getLogger()
 
 
 class FilesystemClient:
@@ -9,8 +12,11 @@ class FilesystemClient:
     def save_ohlcv_config(config, file=OHLCV_CONFIG_FILE):
         with open(file, 'w') as json_file:
             json.dump(config, json_file, indent=2, sort_keys=True)
+        log.info('Successfully saved OHLCV config into {}'.format(OHLCV_CONFIG_FILE))
 
     @staticmethod
     def load_ohlcv_config(file=OHLCV_CONFIG_FILE):
         with open(file) as json_file:
-            return json.load(json_file)
+            config = json.load(json_file)
+        log.info('Successfully loaded OHLCV config from {}'.format(OHLCV_CONFIG_FILE))
+        return config
