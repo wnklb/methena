702b75f8973c35a309bdc1f1d7b9b2ebae595630
Sebastian Winkler <sebastian.winkler@yandex.com>
2020-11-06 18:31:38 +0100

    chore(logging): Added logging to ccxtservice

--- app/services/ccxt.py
+++ app/services/ccxt.py
@@ -27,104 +27,38 @@ class CCXTService(Singleton):
                 await exchange.close()
                 log.info('Successfully closed exchange: {}'.format(exchange_id))
             except Exception as e:
-                log.error('Error during exchange closing!')
+                log.error('Error during closing of exchange!')
                 log.error(e)
             del self.exchanges[exchange_id]
 
-    # async def __aenter__(self):
-    #     await self.__init_exchange_markets(self.state['exchange_ids'])
-    #     return self
-    #
-    # async def __aexit__(self, exc_type, exc_val, exc_tb):
-    #     for exchange_id, exchange in self.exchanges.items():
-    #         try:
-    #             logger.debug('Trying to close exchange: {}'.format(exchange_id))
-    #             await exchange.close()
-    #             logger.info('Successfully closed exchange: {}'.format(exchange_id))
-    #         except Exception as e:
-    #             logger.error('Error during exchange closing!')
-    #             logger.error(e)
-
     async def init_exchange_markets(self, exchange_ids=None):
+        log.info('Initializing exchanges {}'.format(exchange_ids))
         if exchange_ids is None:
             exchange_ids = self.state_service.get_exchanges()
         tasks = [self.__init_exchange_market(exchange_id) for exchange_id in exchange_ids]
         return await asyncio.gather(*tasks)
 
     def build_descriptor(self, raw_descriptor):
+        log.debug('Building descriptor from {}'.format(raw_descriptor))
         exchanges, symbols, timeframes = raw_descriptor
 
-        return {
+        descriptor = {
             exchange: {
                 symbol: self.__get_timeframe_level(exchange, timeframes)
                 for symbol in self.__get_symbol_level(exchange, symbols)
             } for exchange in self.__get_exchange_level(exchanges)
         }
-
-    def __get_exchange_level(self, exchanges):
-        if '*' == exchanges:
-            return ccxt.exchanges
-        return exchanges
-
-    def __get_symbol_level(self, exchange, symbols):
-        if '*' == symbols:
-            return self.exchanges[exchange].symbols
-        return symbols
-
-    def __get_timeframe_level(self, exchange, timeframes):
-        if '*' == timeframes:
-            return self.exchanges[exchange].timeframes
-        return timeframes
-
-    def validate_descriptor(self, descriptor):
-        descriptor_validated = {exchange: {} for exchange in descriptor.keys()}
-        for exchange, symbols in descriptor.items():
-            for symbol, timeframes in symbols.items():
-                if not self.__validate_symbol(exchange, symbol):
-                    log.warning(
-                        'Symbol {} is not available at exchange {}'.format(symbol, exchange))
-                    continue
-                descriptor_validated[exchange][symbol] = []
-                for timeframe in timeframes:
-                    if self.__validate_timeframe(exchange, timeframe):
-                        descriptor_validated[exchange][symbol].append(timeframe)
-                    else:
-                        log.warning('Timeframe {} is not available at {}.{}'.format(
-                            timeframe, exchange, symbol))
-        return descriptor_validated
-
-    def __validate_symbol(self, exchange, symbol):
-        return True if symbol in self.exchanges[exchange].symbols else False
-
-    def __validate_timeframe(self, exchange, timeframe):
-        return True if timeframe in self.exchanges[exchange].timeframes else False
-
-    def get_loaded_exchanges(self):
-        return self.exchanges.values()
-
-    async def __init_exchange_market(self, exchange_id):
-        if exchange_id in self.exchanges:
-            return
-        try:
-            exchange = getattr(ccxt, exchange_id)()
-            await exchange.load_markets()
-            await asyncio.sleep((exchange.rateLimit / 1000) + 0.5)
-            self.exchanges[exchange_id] = exchange
-            log.info('Successfully loaded markets for {} and added them.'.format(exchange_id))
-
-            self.postgres_client.create_exchange_ohlcv_table_if_not_exists(exchange_id)
-        except Exception as e:  # TODO: find out the correct exception.
-            log.error('Unable to load markets for exchange {}.'.format(exchange_id))
-            log.error(e)
-
-    @staticmethod
-    def get_exchanges():
-        return ccxt.exchanges
+        log.debug('Created descriptor {}'.format(descriptor))
+        return descriptor
 
     async def get_exchange(self, exchange_id):
         if exchange_id not in self.exchanges:
             await self.__init_exchange_market(exchange_id)
         return self.exchanges.get(exchange_id)
+
+    @staticmethod
+    def get_exchanges():
+        return ccxt.exchanges
 
     async def get_exchange_symbols(self, exchange_id):
         exchange = await self.get_exchange(exchange_id)
@@ -153,3 +87,61 @@ class CCXTService(Singleton):
     async def get_exchange_timeframes(self, exchange_id):
         exchange = await self.get_exchange(exchange_id)
         return exchange.timeframes
+
+    def get_loaded_exchanges(self):
+        return self.exchanges.values()
+
+    def validate_descriptor(self, descriptor):
+        log.debug('Validating descriptor {}'.format(descriptor))
+        descriptor_validated = {exchange: {} for exchange in descriptor.keys()}
+        for exchange, symbols in descriptor.items():
+            for symbol, timeframes in symbols.items():
+                if not self.__validate_symbol(exchange, symbol):
+                    log.warning('Validation for {}.{} failed. The symbol is not available at the '
+                                'exchange'.format(exchange, symbol))
+                    continue
+                descriptor_validated[exchange][symbol] = []
+                for timeframe in timeframes:
+                    if self.__validate_timeframe(exchange, timeframe):
+                        descriptor_validated[exchange][symbol].append(timeframe)
+                    else:
+                        log.warning('Validation for {}.{}.{} failed. The timeframe is not '
+                                    'available at the exchange'.format(exchange, symbol, timeframe))
+        return descriptor_validated
+
+    @staticmethod
+    def __get_exchange_level(exchanges):
+        if '*' == exchanges:
+            return ccxt.exchanges
+        return exchanges
+
+    def __get_symbol_level(self, exchange, symbols):
+        if '*' == symbols:
+            return self.exchanges[exchange].symbols
+        return symbols
+
+    def __get_timeframe_level(self, exchange, timeframes):
+        if '*' == timeframes:
+            return self.exchanges[exchange].timeframes
+        return timeframes
+
+    async def __init_exchange_market(self, exchange_id):
+        if exchange_id in self.exchanges:
+            return
+        try:
+            exchange = getattr(ccxt, exchange_id)()
+            await exchange.load_markets()
+            await asyncio.sleep((exchange.rateLimit / 1000) + 0.5)
+            self.exchanges[exchange_id] = exchange
+            log.info('Successfully loaded markets for {} and added them.'.format(exchange_id))
+
+            self.postgres_client.create_exchange_ohlcv_table_if_not_exists(exchange_id)
+        except Exception as e:  # TODO: find out the correct exception.
+            log.error('Unable to load markets for exchange {}.'.format(exchange_id))
+            log.error(e)
+
+    def __validate_symbol(self, exchange, symbol):
+        return True if symbol in self.exchanges[exchange].symbols else False
+
+    def __validate_timeframe(self, exchange, timeframe):
+        return True if timeframe in self.exchanges[exchange].timeframes else False
